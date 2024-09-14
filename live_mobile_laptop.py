3717ec423286526ce7c0bf5204fdab83fa80005d
Dhruval Patel <156530333+DhruvalPtl@users.noreply.github.com>
2024-09-14 17:50:40 +0000

    add log

--- live_mobile_laptop.py
+++ live_mobile_laptop.py
@@ -3,7 +3,18 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras.models import load_model
 import streamlit as st
+import logging
 
+# Configure logging
+logging.basicConfig(
+    level=logging.INFO, 
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.FileHandler("app.log"),  # Save logs to a file
+        logging.StreamHandler()          # Output logs to the terminal
+    ]
+)
+# Cache the model so it's only loaded once else error in model server
 @st.cache_resource
 def load_my_model():
     model_path = "mobile_laptop_model/200200_mobile_laptop_classifier0.916.keras"
@@ -23,11 +34,12 @@ def live_object_detection(model):
         st.session_state['run'] = False
     
     vid = cv2.VideoCapture(0)
+    
     while st.session_state['run']:
         ret, frame = vid.read()
         
         if not ret:
-            print("faild to connect camera")
+            logging.info("faild to connect camera")
             break
         
         img = cv2.resize(frame,(200,200))
@@ -36,16 +48,19 @@ def live_object_detection(model):
         img_to_array /= 255
         
         predi = model.predict(img_to_array)
-        print(f"Prediction: {predi}")
+        logging.info(f"Prediction: {predi}")
         
         if predi > 0.5:
             cv2.putText(frame,"Mobile",(50,50),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
+            logging.info(f"Prediction: Mobile")
         else:
             cv2.putText(frame,"Laptop",(50,50),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
+            logging.info(f"Prediction: Laptop")
         
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         frame_holder.image(frame, channels="RGB")    
         
+        cv2.waitKey(100) # to set frame per second (delay)
         if not st.session_state['run']:
             break
         
